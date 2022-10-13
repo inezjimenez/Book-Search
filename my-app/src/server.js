deabd3b31d4f1485da24c19e5e73578d967fa69f
Inez Jimenez <ijim9182@yahoo.com>
2022-10-13 14:41:04 -0700

    created server end folders

---
+++ my-app/src/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+
+const {ApolloServer} = require('apollo-server-express');
+
+// typeDefs and resolvers
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
+
+
+const db = require('./config/connection');
+//const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
